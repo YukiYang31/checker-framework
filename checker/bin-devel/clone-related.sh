7c22e0fec1aa18b5eaae2bb33a4c365213bfe41f
Michael Ernst <mernst@cs.washington.edu>
2026-04-12 21:53:59 -0700

    Change location for cloning the JDK

--- checker/bin-devel/clone-related.sh
+++ checker/bin-devel/clone-related.sh
@@ -49,7 +49,7 @@ else
 fi
 
 # Clone the annotated JDK into ../jdk .
-"$GIT_SCRIPTS/git-clone-related" ${DEBUG_FLAG} typetools jdk
+"$GIT_SCRIPTS/git-clone-related" ${DEBUG_FLAG} typetools ../jdk
 
 ### Commented temporarily because JSpecify build is failing under JDK 17.
 ### (I guess they don't use continuous integration.)
