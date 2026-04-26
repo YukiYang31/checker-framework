28a16c5ecb7b0d9372db7d480a99239f51eb15c9
YukiYang31 <yukyang@cs.washington.edu>
2026-04-26 16:19:43 -0700

    annotate modifiable collections as @IteratorPreserveRemove; add tests

--- checker/tests/modifiability/IteratorPrecisionTest.java
+++ checker/tests/modifiability/IteratorPrecisionTest.java
@@ -1,15 +1,35 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.EnumSet;
+import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.TransferQueue;
 import org.checkerframework.checker.modifiability.qual.Modifiable;
 import org.checkerframework.checker.modifiability.qual.Shrinkable;
 import org.checkerframework.checker.modifiability.qual.Unshrinkable;
 
 public class IteratorPrecisionTest {
+
+  enum TestEnum {
+    A,
+    B
+  }
 
   void arrayListIterator() {
     // ok
@@ -69,5 +89,58 @@ public class IteratorPrecisionTest {
 
     // The iterator supports remove(), but the type is treated as unknown shrinkability.
     @Shrinkable Iterator<String> lost = keys2.iterator();
+  }
+
+  void setIteratorPreservesRemove() {
+    Set<String> hashSet = new HashSet<>();
+    @Shrinkable Iterator<String> hashSetIterator = hashSet.iterator();
+    hashSetIterator.remove();
+
+    Set<String> treeSet = new TreeSet<>();
+    @Shrinkable Iterator<String> treeSetIterator = treeSet.iterator();
+    treeSetIterator.remove();
+  }
+
+  void dequeIteratorPreservesRemove() {
+    Deque<String> arrayDeque = new ArrayDeque<>();
+    @Shrinkable Iterator<String> arrayDequeIterator = arrayDeque.iterator();
+    arrayDequeIterator.remove();
+
+    Deque<String> linkedList = new LinkedList<>();
+    @Shrinkable Iterator<String> linkedListIterator = linkedList.iterator();
+    linkedListIterator.remove();
+  }
+
+  void queueIteratorPreservesRemove() {
+    Queue<String> priorityQueue = new PriorityQueue<>();
+    @Shrinkable Iterator<String> priorityQueueIterator = priorityQueue.iterator();
+    priorityQueueIterator.remove();
+
+    BlockingQueue<String> arrayBlockingQueue = new ArrayBlockingQueue<>(1);
+    @Shrinkable Iterator<String> arrayBlockingQueueIterator = arrayBlockingQueue.iterator();
+    arrayBlockingQueueIterator.remove();
+
+    BlockingQueue<String> linkedBlockingQueue = new LinkedBlockingQueue<>();
+    @Shrinkable Iterator<String> linkedBlockingQueueIterator = linkedBlockingQueue.iterator();
+    linkedBlockingQueueIterator.remove();
+
+    TransferQueue<String> linkedTransferQueue = new LinkedTransferQueue<>();
+    @Shrinkable Iterator<String> linkedTransferQueueIterator = linkedTransferQueue.iterator();
+    linkedTransferQueueIterator.remove();
+  }
+
+  void factoryIteratorPreservesRemove() {
+    Set<TestEnum> enumSet = EnumSet.of(TestEnum.A, TestEnum.B);
+    @Shrinkable Iterator<TestEnum> enumSetIterator = enumSet.iterator();
+    enumSetIterator.remove();
+
+    Set<TestEnum> enumSetCopy = EnumSet.copyOf(enumSet);
+    @Shrinkable Iterator<TestEnum> enumSetCopyIterator = enumSetCopy.iterator();
+    enumSetCopyIterator.remove();
+
+    Enumeration<String> enumeration = Collections.enumeration(List.of("a", "b"));
+    List<String> list = Collections.list(enumeration);
+    @Shrinkable Iterator<String> listIterator = list.iterator();
+    listIterator.remove();
   }
 }
